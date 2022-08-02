89a84db5c01d5e47c21cfaca034a0809a409ee2b
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-08-02 17:40:16 -0400

    Practice done

---
+++ API_Practice/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+
+
+var client = new HttpClient();
+
+
+
+var kanyeURL = "https://api.kanye.rest/";
+
+
+var ronSwansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+
+
+
+
+for (int i = 0; i < 5; i++)
+{
+
+
+    var ronResponse = client.GetStringAsync(ronSwansonURL).Result;
+
+
+    var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+
+    var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Replace('"',' ').Trim();
+    string kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+    Console.WriteLine(ronQuote);
+
+    Console.WriteLine(kanyeQuote);
+}
+
+
